7aeaef87e471be86e40b63320030f4fa2e6d7b76
rmestre <raf3102@gmail.com>
2026-04-03 15:04:45 +0200

    ControlVerifierIdentiteTest done

---
+++ test/controleur/ControlVerifierIdentiteTest.java
@@ -0,0 +1,39 @@
+package controleur;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import personnages.Chef;
+import personnages.Gaulois;
+import villagegaulois.Village;
+
+class ControlVerifierIdentiteTest {
+	private Village village;
+	private Chef abraracourcix;
+	
+	@BeforeEach
+	public void initialiserSituation() {
+		System.out.println("Initialisation...");
+		village = new Village("Le village des irréductibles", 10, 5);
+		abraracourcix = new Chef("Abraracourcix", 10, village);
+		village.setChef(abraracourcix);
+	}
+
+	@Test
+	void testControlVerifierIdentite() {
+		ControlVerifierIdentite controlVerifierIdentite = new ControlVerifierIdentite(village);
+		assertNotNull(controlVerifierIdentite, "Constructeur ne renvoie pas null");
+	}
+
+	@Test
+	void testVerifierIdentite() {
+		ControlVerifierIdentite controlVerifierIdentite = new ControlVerifierIdentite(village);
+		village.ajouterHabitant(new Gaulois("Bonemine",10));
+		assertTrue(controlVerifierIdentite.verifierIdentite("Bonemine"));
+		assertFalse(controlVerifierIdentite.verifierIdentite("César"));
+		assertTrue(controlVerifierIdentite.verifierIdentite("Abraracourcix"));
+	}
+
+}
